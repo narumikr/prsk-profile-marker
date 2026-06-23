92f552db01497b0a9ac60ef2b8e355ba7f81a9c9
Narumikr <narunarumikumiku396@gmail.com>
2026-06-23 23:06:38 +0900

    add className props

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,13 +1,14 @@
 interface InputProps {
+  className?: string;
   label: string;
 }
 
-export const Input = ({ label }: InputProps) => {
+export const Input = ({ label, className }: InputProps) => {
   return (
     <input
       type="text"
       aria-label={label}
-      className="w-full font-bold rounded border-0 px-2 py-1 text-center outline-none focus:border-0 focus:outline-none focus:ring-0"
+      className={`w-full font-bold rounded border-0 px-2 py-1 text-center outline-none focus:border-0 focus:outline-none focus:ring-0 ${className}`}
     />
   );
 };
